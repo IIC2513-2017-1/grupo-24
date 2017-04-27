4a5a38c0fc7771789af317b3ecb827f5bdaee3de
Juan Ignacio Cortes <jicortes2@uc.cl>
2017-04-27 18:06:19 -0300

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
   has_many :projects
 #  has_many :donations, class_name: 'Project', foreign_key: 'project_id',
-  validates :username, :email, :password, presence: true
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  validates :username, :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 4 }
+
+  def full_name
+    return "#{name} #{last_name}" if name && last_name
+    return "#{name}" if first_name
+    return "#{last_name}" if last_name
+    email
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :projects
   resources :categories
   resources :users
+  resource :session, only: [:new, :create, :destroy]
   get '/buscar/:search', to: 'home#search', as: 'search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
