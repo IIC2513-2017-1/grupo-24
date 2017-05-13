cdb8b26b741e73202e77a3cabc8666b75d1540f9
Juan Ignacio Cortes <jicortes2@uc.cl>
2017-05-13 01:19:28 -0300

    avatar update on change

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).on('turbolinks:load page:restore', function(){
+  $('#user_avatar').on('change', function(event) {
+    var files = event.target.files;
+    var image = files[0]
+    var reader = new FileReader();
+    reader.onload = function(file) {
+      var img = new Image();
+      img.src = file.target.result;
+      $('.edit-avatar').attr('src', img.src);
+    }
+    reader.readAsDataURL(image);
+  });
+
+});
