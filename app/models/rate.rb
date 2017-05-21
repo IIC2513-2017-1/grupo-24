3a5e155f62ad24a6730d85b20af8f8bb0246b771
Juan Ignacio Cortes <jicortes2@uc.cl>
2017-05-20 20:32:51 -0400

    fix donation show

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,4 +1,8 @@
 class Rate < ApplicationRecord
   belongs_to :user
   belongs_to :project
+
+  validate :grade, numericality: { less_than_or_equal_to: 5,
+                                   gretear_than_or_equal_to: 0,
+                                   only_integer: true }
 end
