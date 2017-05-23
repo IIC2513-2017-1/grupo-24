93f25c501280a0c1ca69289b1fe262d616c831bd
Juan Ignacio Cortes <jicortes2@uc.cl>
2017-05-22 21:04:09 -0400

    category admin

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
     resources :projects
   end
   root 'home#main'
-  resources :categories
   resources :users do
     resources :projects, only: [:index]
     resources :donations, only: [:index]
