b3a328fbdcea3cf4946d741348a20c5786ac476e
Juan Ignacio Cortes <jicortes2@uc.cl>
2017-05-27 22:08:32 -0400

    filter working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+
   def main
     @projects = Project.all.order(id: :desc).limit(3)
   end
@@ -11,15 +12,12 @@ class HomeController < ApplicationController
   end
 
   def category_filter
-    puts '########################################3', 'AQUIII'
-    @last_search = params[:search]
-    @categories = Category.all
     @projects = Project.where("title ILIKE :search AND publish = true",
                               search: "%#{params[:search]}%")
                        .select {|project| params[:filters].include?(project.category.name)}
-    puts @projects.size
     respond_to do |format|
-      format.js
+      format.js { render layout: false }
+      format.html {head :no_content }
     end
   end
 end
