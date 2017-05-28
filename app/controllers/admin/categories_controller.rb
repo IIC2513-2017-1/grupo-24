a092803859d89c4dc7d3098ca31bfc87dcfb58c1
Juan Ignacio Cortes <jicortes2@uc.cl>
2017-05-28 16:36:36 -0400

    links fixed

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -32,7 +32,7 @@ module Admin
 
       respond_to do |format|
         if @category.save
-          format.html { redirect_to admin_category_path(@category), notice: 'Category was successfully created.' }
+          format.html { redirect_to admin_categories_path, notice: 'Categoría creada correctamente.' }
           format.json { render :show, status: :created, location: @category }
         else
           format.html { render :new }
@@ -46,7 +46,7 @@ module Admin
     def update
       respond_to do |format|
         if @category.update(category_params)
-          format.html { redirect_to admin_category_path(@category), notice: 'Category was successfully updated.' }
+          format.html { redirect_to admin_categories_path, notice: 'Categoría actualizada correctamente.' }
           format.json { render :show, status: :ok, location: @category }
         else
           format.html { render :edit }
