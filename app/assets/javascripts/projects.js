44994a1506e2e13305d1cd696f49361ebbb9f92d
Juan Ignacio Cortes <jicortes2@uc.cl>
2017-06-11 21:02:55 -0400

    progress bar

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function move(percentege) {
+    var elem = document.getElementById("myBar");
+    var width = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100 || width == percentege) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
+
+$(document).on("turbolinks:load", function() {
+  move($('#myBar').data('percentage'));
+});
