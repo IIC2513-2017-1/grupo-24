4d8711ac54a10e1d0daae1175b8288a8d6bdf5ef
Juan Ignacio Cortes <jicortes2@uc.cl>
2017-06-11 22:57:13 -0400

    host without http

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "all_4_one_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => "https://all-for-one.herokuapp.com/" }
+  config.action_mailer.default_url_options = { :host => "all-for-one.herokuapp.com" }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
